cd2f81fcd0df5a5e9353ed6b64e03f85908b4638
laramcd <61915160+laramcd@users.noreply.github.com>
2023-06-21 13:38:37 +0200

    make groceryList persistent

--- app/src/main/java/com/nlinterface/activities/GroceryListActivity.kt
+++ app/src/main/java/com/nlinterface/activities/GroceryListActivity.kt
@@ -2,21 +2,23 @@ package com.nlinterface.activities
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nlinterface.R
-import com.nlinterface.databinding.ActivityGroceryListBinding
 import com.nlinterface.adapters.GroceryListAdapter
+import com.nlinterface.databinding.ActivityGroceryListBinding
 import com.nlinterface.dataclasses.GroceryItem
 import com.nlinterface.viewmodels.GroceryListViewModel
+
 
 class GroceryListActivity : AppCompatActivity() {
 
@@ -50,7 +52,23 @@ class GroceryListActivity : AppCompatActivity() {
             onAddItemButtonClick()
         }
 
-        // RecyclerView GroceryItem Listener
+        val voiceActivationButton = findViewById<View>(R.id.grocery_list_bt) as ImageButton
+        val width = resources.displayMetrics.widthPixels
+        val height = resources.displayMetrics.heightPixels / 3
+        voiceActivationButton.layoutParams = ConstraintLayout.LayoutParams(width, height)
+
+        voiceActivationButton.setOnClickListener {
+            onAddVoiceActivationButtonClick()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.storeGroceryList()
+    }
+
+    private fun onAddVoiceActivationButtonClick() {
+        Log.println(Log.ASSERT, "GroceryListActivity: onAddVoiceActivationButtonClick", "Button CLicked")
     }
 
     private fun onAddItemButtonClick() {
@@ -62,8 +80,8 @@ class GroceryListActivity : AppCompatActivity() {
             builder.apply {
                 setPositiveButton(R.string.add) { _, _ ->
 
-                    var addItemEt = view.findViewById<EditText>(R.id.add_item_et)
-                    var newItemName = addItemEt.text.toString()
+                    val addItemEt = view.findViewById<EditText>(R.id.add_item_et)
+                    val newItemName = addItemEt.text.toString()
 
                     viewModel.addGroceryItem(newItemName)
                     adapter.notifyItemInserted(groceryItemList.size - 1)
@@ -78,6 +96,5 @@ class GroceryListActivity : AppCompatActivity() {
         }
         alertDialog?.show()
     }
-
 
 }
--- app/src/main/java/com/nlinterface/viewmodels/GroceryListViewModel.kt
+++ app/src/main/java/com/nlinterface/viewmodels/GroceryListViewModel.kt
@@ -1,22 +1,52 @@
 package com.nlinterface.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.nlinterface.dataclasses.GroceryItem
+import java.io.BufferedReader
+import java.io.File
 
-class GroceryListViewModel : ViewModel() {
 
+class GroceryListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val context = application
+    private val groceryListFileName = "GroceryList.json"
+    private val groceryListFile: File = File(context.filesDir, groceryListFileName)
     var groceryList: ArrayList<GroceryItem> = ArrayList<GroceryItem>()
+    var gson = Gson()
 
     fun fetchGroceryList() {
-        groceryList = arrayListOf(GroceryItem("Apples"),
-            GroceryItem("Oranges"), GroceryItem("Bananas"))
+
+        if (!groceryListFile.exists()) {
+            groceryListFile.createNewFile()
+        }
+
+        if (groceryListFile.length() > 0) {
+            val bufferedReader: BufferedReader = groceryListFile.bufferedReader()
+            val inputString = bufferedReader.use { it.readText() }
+            groceryList = gson.fromJson(
+                inputString,
+                object : TypeToken<ArrayList<GroceryItem?>?>() {}.type
+            ) as ArrayList<GroceryItem>
+        }
+
+        Log.println(Log.DEBUG, "1", groceryList.toString())
     }
 
     fun addGroceryItem(itemName: String): ArrayList<GroceryItem> {
-        groceryList.add(GroceryItem(itemName))
+        groceryList.add(GroceryItem(itemName, groceryList.size, false))
         return groceryList
     }
 
+    fun deleteGroceryItem(groceryItem: GroceryItem) {
+        groceryList.remove(groceryItem)
+    }
 
-
+    fun storeGroceryList() {
+        val jsonString : String = gson.toJson(groceryList)
+        groceryListFile.writeText(jsonString)
+    }
 }

8b15021d7ce7b744f6723b62dea592d87096963b
laramcd <61915160+laramcd@users.noreply.github.com>
2024-01-02 16:39:19 +0100

    javadoc and clarifying comments for utilities

--- app/src/main/java/com/nlinterface/utility/TextToSpeechUtility.kt
+++ app/src/main/java/com/nlinterface/utility/TextToSpeechUtility.kt
@@ -4,27 +4,55 @@ import android.content.Context
 import android.speech.tts.TextToSpeech
 import java.util.Locale
 
-class TextToSpeechUtility(context: Context, listener: OnInitListener)
-    : TextToSpeech(context, listener) {
+/**
+ * /**
+ *  * Helper class for android.speech.tts.TextToSpeech.
+ *  */
+ */
+class TextToSpeechUtility(context: Context, listener: OnInitListener) :
+    TextToSpeech(context, listener) {
 
     private var textToSpeechEngine: TextToSpeech = TextToSpeech(context, listener)
 
+    /**
+     * Set the engine language to the given locale.
+     *
+     * @param locale: Locale, the Locale to be used
+     */
     fun setLocale(locale: Locale = Locale.getDefault()) {
         textToSpeechEngine.language = locale
     }
 
+    /**
+     * Sets the engine speech speed rate.
+     *
+     * @param speedRate: Float, the desired speed rate
+     */
     fun setSpeedRate(speedRate: Float = 1.2F) {
         textToSpeechEngine.setSpeechRate(speedRate)
     }
 
+    /**
+     * Reads a string out loud.
+     *
+     * @param text: String to be read out loud
+     * @param queueMode: Int, defines whether to append the text to the speaking queue (QUEUE.ADD)
+     * or to overwrite the queue with the text (QUEUE.FLUSH)
+     */
     fun say(text: String, queueMode: Int) {
         textToSpeechEngine.speak(text, queueMode, null, "tts1")
     }
 
+    /**
+     * Stops the speech engine if the activity is paused.
+     */
     fun onPause() {
         textToSpeechEngine.stop()
     }
 
+    /**
+     * Destroys the speech engine at the end of the activity lifecycle.
+     */
     fun onDestroy() {
         textToSpeechEngine.shutdown()
     }
--- app/src/main/java/com/nlinterface/utility/utils.kt
+++ app/src/main/java/com/nlinterface/utility/utils.kt
@@ -1,38 +1,53 @@
 package com.nlinterface.utility
 
-import android.content.Context
 import android.content.res.Resources
-import android.os.Parcelable
 import android.util.Log
 import android.view.View
-import androidx.annotation.AttrRes
-import androidx.annotation.ColorInt
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.nlinterface.dataclasses.GroceryItem
-import java.io.BufferedReader
-import java.io.File
-import java.util.Dictionary
 import java.util.Locale
 
+/**
+ * Resize a View's width relative to the screen size.
+ *
+ * @param view: View to be resized
+ * @param relWidth: Double, the desired relative width
+ */
 fun setViewRelativeWidth(view: View, relWidth: Double) {
     val width: Int = (Resources.getSystem().displayMetrics.widthPixels * relWidth).toInt()
     view.layoutParams.width = width
 }
 
+/**
+ * Resize a View's height relative to the screen size.
+ *
+ * @param view: View to be resized
+ * @param relHeight: Double, the desired relative height
+ */
 fun setViewRelativeHeight(view: View, relHeight: Double) {
     val height = (Resources.getSystem().displayMetrics.heightPixels * relHeight).toInt()
     view.layoutParams.height = height
 }
 
-fun setViewRelativeSize(view: View, relWidth:Double, relHeight: Double) {
+/**
+ * Resize a View's width and height relative to the screen size.
+ *
+ * @param view: View to be resized
+ * @param relWidth: Double, the desired relative width
+ * @param relHeight: Double, the desired relative height
+ */
+fun setViewRelativeSize(view: View, relWidth: Double, relHeight: Double) {
     val height = (Resources.getSystem().displayMetrics.heightPixels * relHeight).toInt()
     val width = (Resources.getSystem().displayMetrics.widthPixels * relWidth).toInt()
     view.layoutParams = ConstraintLayout.LayoutParams(width, height)
 }
 
-fun getLocaleType() : LocaleType {
+/**
+ * Evaluate the currently used Locale to a Locale Type
+ *
+ * @return LocaleType.EN, if default is english, LocaleType.DE, if default is german,
+ * else LocaleType.OTHER
+ */
+fun getLocaleType(): LocaleType {
 
     return if (Locale.getDefault().toString().contains("en", true)) {
         LocaleType.EN
@@ -42,69 +57,4 @@ fun getLocaleType() : LocaleType {
         LocaleType.OTHER
     }
 
-}
-
-fun cleanSTTInput(input: String) : String {
-
-    val punctuationRegex = Regex("[^\\w\\süäöß]")
-
-    var cleanInput = input.lowercase()
-    cleanInput = cleanInput.replace(punctuationRegex, "")
-
-    cleanInput = if (getLocaleType() == LocaleType.EN) {
-        cleanVoiceInputEN(cleanInput)
-    } else if (getLocaleType() == LocaleType.DE) {
-        cleanVoiceInputDE(cleanInput)
-    } else {
-        Log.println(Log.DEBUG, "cleanVoiceInput", "Locale = Locale.OTHER")
-        cleanVoiceInputEN(cleanInput)
-    }
-
-    return cleanInput
-
-}
-
-private fun cleanVoiceInputEN(input: String): String {
-
-    val word2Digit = mapOf(
-        "a" to 1,
-        "an" to 1,
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6
-    )
-
-    return input.replace(word2Digit)
-
-}
-
-private fun cleanVoiceInputDE(input: String): String {
-
-    val word2Digit = mapOf(
-        "ein" to 1,
-        "eine" to 1,
-        "einen" to 1,
-        "zwei" to 2,
-        "drei" to 3,
-        "view" to 4,
-        "fünf" to 5,
-        "sechs" to 6)
-
-    return input.replace(word2Digit)
-
-}
-
-private fun String.replace(map: Map<String, Int>): String {
-    var result = this
-    map.forEach { (k, v) -> result = result.replace(k, v) }
-    return result
-}
-
-private fun String.replace(k: String, v: Int): String {
-    val result = this
-    result.replace(k, v.toString())
-    return result
 }
